e04305ab616010560c250cbbccad223f4afee580
jrvaghela72 <jrvaghela72@gmail.com>
2016-12-10 17:06:22 +0530

    third

--- Book1/MainFrame.cpp
+++ Book1/MainFrame.cpp
@@ -18,7 +18,7 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	if(CFrameWnd::OnCreate(lpCreateStruct)==0)
 	{
 		//IF WINDOWS CREATED SUCCESSFULLY,THEN LET USER KNOW
-		MessageBox(_T("The windows has been created successfully"));
+		//MessageBox(_T("The windows has been created successfully"));
 		//IF WINDOWS WILL CREATED SUCCESSFULLY THEN IT WILL RETURN 0
 		return 0;
 	}
@@ -73,7 +73,83 @@ void CMainFrame::OnActivate(UINT nState, CWnd * pWndOther, BOOL bMinimised)
 void CMainFrame::OnPaint()
 {
 	CFrameWnd::OnPaint();
-	MessageBox(_T("The windows has been printed"));
+	//MessageBox(_T("The windows has been printed"));
+}
+
+//THIS METHOD WILL SHOW THE WINDOWS SIZING BEHAVIOUR 
+void CMainFrame::OnSize(UINT nType, int cx, int cy)
+{
+	CFrameWnd::OnSize(nType, cx, cy);
+	//VARIABLE FOR HANDLING THE MESSAGEBOX TEXT
+	
+	CString msg;
+	
+	switch(nType)
+	{
+	case SIZE_MAXIMIZED:
+		msg.Format(L"Maximized CX :%d,CY:%d",cx,cy);
+		break;
+	case SIZE_MINIMIZED:
+		msg.Format(L"Minimized CX :%d,CY:%d", cx, cy);
+		break;
+	case SIZE_RESTORED:
+		msg.Format(L"Restored CX :%d,CY:%d", cx, cy);
+		break;
+	case SIZE_MAXHIDE:
+		msg.Format(L"Maximized not me CX :%d,CY:%d", cx, cy);
+		break;
+	case SIZE_MAXSHOW:
+		msg.Format(L"Restored not me CX :%d,CY:%d", cx, cy);
+		break;
+	}
+	//MessageBox(msg);
+
+	
+	
+}
+//IF WE RESIZE THE WINDOWS LIKE MAKE IT SMALL USING MOUSE DRAGING ON THE CORNER
+void CMainFrame::OnSizing(UINT nSide, LPRECT lpRect)
+{
+	CString msg;
+
+	long btm = lpRect->bottom;
+	long tp = lpRect->top;
+	long lf = lpRect->left;
+	long rt = lpRect->right;
+
+	switch(nSide)
+	{ 
+	case WMSZ_BOTTOM:
+		msg.Format(L"Maximized bottom :%ld,top:%ld,left=%ld,right=%ld", btm, tp,lf,rt);
+		break;
+	case WMSZ_BOTTOMLEFT:
+		msg.Format(L"Maximized bottom :%ld,top:%ld,left=%ld,right=%ld", btm, tp, lf, rt);
+		break;
+	case WMSZ_BOTTOMRIGHT:
+		msg.Format(L"Maximized bottom :%ld,top:%ld,left=%ld,right=%ld", btm, tp, lf, rt);
+		break;
+	case WMSZ_RIGHT:
+		msg.Format(L"Maximized bottom :%ld,top:%ld,left=%ld,right=%ld", btm, tp, lf, rt);
+		break;
+	case WMSZ_LEFT:
+		msg.Format(L"Maximized bottom :%ld,top:%ld,left=%ld,right=%ld", btm, tp, lf, rt);
+		break;
+	case WMSZ_TOP:
+		msg.Format(L"Maximized bottom :%ld,top:%ld,left=%ld,right=%ld", btm, tp, lf, rt);
+		break;
+	case WMSZ_TOPLEFT:
+		msg.Format(L"Maximized bottom :%ld,top:%ld,left=%ld,right=%ld", btm, tp, lf, rt);
+		break;
+	case WMSZ_TOPRIGHT:
+		msg.Format(L"Maximized bottom :%ld,top:%ld,left=%ld,right=%ld", btm, tp, lf, rt); 
+		break;
+	}
+	//MessageBox(msg);
+}
+
+void CMainFrame::OnDestroy()
+{
+	//MessageBox(_T("Window will be destroyed"));
 }
 
 //THIS MACRO DECLARATION WILL CREATE THE COMMUNICATION BETWEEN THE CMainFrame and CFrameWmd{base class of th CMainFrame header will declare in MainFrame.h file}
@@ -83,6 +159,9 @@ BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
 	ON_WM_SHOWWINDOW()//THIS WILL USE THE OnShowWindow
 	ON_WM_ACTIVATE()//THIS WILL USE THE OnActivate
 	ON_WM_PAINT()//THIS WILL USE THE OnPaint
+	ON_WM_SIZE()//THIS WILL USE THE OnSize
+	ON_WM_SIZING()//THIS WILL USE THE OnSizing
+	ON_WM_DESTROY()//THIS WILL USE OnDestroy
 END_MESSAGE_MAP()
 
 //this class will show the windows behaviour
--- Book1/MainFrame.h
+++ Book1/MainFrame.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<afxwin.h>
+
 class CMainFrame :	public CFrameWnd
 {
 
@@ -11,6 +12,9 @@ protected:
 	afx_msg void OnShowWindow(BOOL bShow,UINT nStatus);
 	afx_msg void OnActivate(UINT nState, CWnd * pWndOther, BOOL bMinimised);
 	afx_msg void OnPaint();
+	afx_msg void OnSize(UINT nType,int cx,int cy);
+	afx_msg void OnSizing(UINT nSide,LPRECT lpRect);
+	afx_msg void OnDestroy();
 	DECLARE_MESSAGE_MAP()
 };
 
